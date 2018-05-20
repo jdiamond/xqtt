137645749cffd5a8372fa2c20f0283315499e432
Jason Diamond <jason@diamond.name>
2018-05-19 20:13:15 -0700

    publish decode test

--- packages/xqtt/src/packets/publish.test.js
+++ packages/xqtt/src/packets/publish.test.js
@@ -2,7 +2,7 @@
 
 /* eslint-env jest */
 
-import { encode } from './index';
+import { encode, decode } from './index';
 
 describe('packets', () => {
   describe('publish', () => {
@@ -33,6 +33,50 @@ describe('packets', () => {
           97, // 'a'
           100, // 'd'
         ]);
+      });
+    });
+
+    describe('decode', () => {
+      test('it works', () => {
+        expect(
+          decode(
+            Uint8Array.from([
+              // fixedHeader
+              48, // packetType + flags
+              12, // remainingLength
+              // variableHeader
+              0, // topicLength MSB
+              3, // topicLength LSB
+              97, // 'a'
+              47, // '/'
+              98, // 'b'
+              // payload
+              112, // 'p'
+              97, // 'a'
+              121, // 'y'
+              108, // 'l'
+              111, // 'o'
+              97, // 'a'
+              100, // 'd'
+            ])
+          )
+        ).toEqual({
+          type: 'publish',
+          dup: false,
+          qos: 0,
+          retain: false,
+          id: null,
+          topic: 'a/b',
+          payload: Uint8Array.from([
+            112, // 'p'
+            97, // 'a'
+            121, // 'y'
+            108, // 'l'
+            111, // 'o'
+            97, // 'a'
+            100, // 'd'
+          ]),
+        });
       });
     });
   });
