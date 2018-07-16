bfcc1962f372c921ab6140f1139b6fea9f50b549
Jason Diamond <jason@diamond.name>
2018-07-15 17:12:00 -0700

    unsubscribe

--- packages/cqtt/src/shell.js
+++ packages/cqtt/src/shell.js
@@ -1,12 +1,19 @@
+import fs from 'fs';
+import { promisify } from 'util';
 import vorpal_ from 'vorpal';
+import untildify from 'untildify';
 import { connect } from './connect';
 
+const readFile = promisify(fs.readFile);
+
 const vorpal = vorpal_();
+
+const notConnectedPrompt = '(not connected)';
 
 export function run(argv) {
   init(argv);
 
-  vorpal.delimiter('nqtt$').show();
+  vorpal.delimiter(`${notConnectedPrompt}:`).show();
 }
 
 function init(argv) {
@@ -16,19 +23,30 @@ function init(argv) {
 
   vorpal
     .command('connect', 'Connect to a broker.')
-    .option('-h, --host', 'broker host')
-    .option('-p, --port', 'broker port')
+    .option('-h, --host <host>', 'broker host')
+    .option('-p, --port <port>', 'broker port')
     .option('--tls', 'use TLS')
-    .option('--ca', 'path to CA certificate')
+    .option('--ca <ca>', 'path to CA certificate')
+    .option('-u, --username <username>', 'username')
+    .option('-P, --password <password>', 'password')
     .action(async function(args) {
       const host = args.options.host || argv.host || 'localhost';
       const port = args.options.port || argv.port || 1883;
+
+      vorpal.delimiter(`${host}:`);
 
       this.log(`connecting to host ${host}, port ${port}`);
 
       client = await connect({
         host,
         port,
+        tls: args.options.tls
+          ? {
+              ca: args.options.ca ? await readFile(untildify(args.options.ca)) : null,
+            }
+          : false,
+        username: args.options.username,
+        password: args.options.password,
         logger: msg => {
           if (msg.startsWith('packet ') && !logPackets) {
             return;
@@ -45,8 +63,17 @@ function init(argv) {
             vorpal.log(msg);
           }
         },
+        onmessage(packet) {
+          console.log(packet); // eslint-disable-line no-console
+        },
       });
     });
+
+  vorpal.command('disconnect', 'Disconnect from broker.').action(function(_args) {
+    client.disconnect();
+
+    vorpal.delimiter(`${notConnectedPrompt}:`);
+  });
 
   vorpal
     .command('publish <topic> [msg]', 'Publish to topic.')
@@ -66,6 +93,15 @@ function init(argv) {
       this.log(`subscribing to ${args.topic}`);
 
       await client.subscribe(args.topic, +args.options.qos || 0);
+    });
+
+  vorpal
+    .command('unsubscribe <topic>', 'Unsubscribe from topics.')
+    .alias('unsub')
+    .action(async function(args) {
+      this.log(`unsubscribing from ${args.topic}`);
+
+      client.unsubscribe(args.topic);
     });
 
   vorpal
